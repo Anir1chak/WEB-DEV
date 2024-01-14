7de3ca1a90fb9c9d55f12dc7b014928ad76c640c
Anirvan <anirvanchakravarty39@gmail.com>
2024-01-15 01:01:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Tictactoe from './components/Tictactoe/Tictactoe.jsx'
+
+function App() {
+  return (
+    <div>
+      
+      <Tictactoe/>
+    </div>
+  );
+}
+
+export default App;
